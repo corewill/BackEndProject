01b76430c3e604fbf2639f98da6748fbeb4af02c
stoicgit <118203310+stoicgit@users.noreply.github.com>
2023-01-27 15:32:10 -0500

    Update index.js

--- CarwashApp/index.js
+++ CarwashApp/index.js
@@ -1,52 +1,107 @@
 const { createClient } = require("@supabase/supabase-js");
-const supabaseUrl = "https://pyymrehpabqxdbuobtbh.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5eW1yZWhwYWJxeGRidW9idGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ1OTA3MjgsImV4cCI6MTk5MDE2NjcyOH0.GPJW32brXyjktYJiinHapS82d4GVRlSUrv06gx9fBA0";
+const supabaseUrl = "https://hrhviqtzmgmsttkvotvq.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyaHZpcXR6bWdtc3R0a3ZvdHZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ2NjIzNjYsImV4cCI6MTk5MDIzODM2Nn0.AiiB8o314C_rkOdneQk_vwvzxXO4W3lwHN-8-_NKEsA";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const express = require("express")
-const app = express()
-const PORT = 3026
-// const bcrypt = require("bcrypt")
-// const session = require("express-session")
-// const cookieParser = require("cookie-parser")
-// const bodyParser = require("body-parser");
-
-
-app.use(express.json())
-
-// app.use(
-//     bodyParser.urlencoded({
-//         extended: true,
-//     })
-// )
-
-// app.use(bodyParser.json())
-// app.use(cookieParser())
-// app.use(session({
-//     secret: 'secret',
-//     resave: false,
-//     saveUninitialize: true,
-//     cookie: {
-//         secure: false,
-//         maxAge: 2592000000
-//     }
-// }))
-app.set("view engine", "ejs")
+const express = require("express");
+const app = express();
+const PORT = 3026;
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 
 app.get("/login", (req, res) => {
-    res.render("pages/login")
-})
-app.post("/login", async (req, res) => {
-    const {userName, passWord,firstName,lastName} = req.body
-    console.log(req.body)
-  console.log("route triggered")
-    const { data, error } = await supabase
-    .from('Members')
-    .insert([
-      { userName, firstName, lastName, passWord },
-    ])
-  console.log(error)
-    res.send(data)
-})
+  res.render("pages/login");
+});
 
-app.listen(PORT, console.log(`Listening on port ${PORT}`))
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+  if (data) {
+    res.redirect("login");
+    return;
+  } else {
+    res.send("Error signing up");
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (data) {
+      res.render("pages/index", { user: data });
+      return;
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.post("/Appointments", async (req, res) => {
+  const { date, customer_id, package_id } = req.body;
+  try {
+    const { data, error } = await supabase.from("Appointements").insert([
+      {
+        date,
+        customer_id,
+        package_id,
+      },
+    ]);
+    if (data) {
+      res.status(200).render("pages/index", { user: data });
+      return;
+    } else {
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.post("/Packages", async (req, res) => {
+  const { name, description, Price } = req.body;
+  try {
+    const { data, error } = await supabase.from("Packages").insert([
+      {
+        name,
+        description,
+        Price,
+      },
+    ]);
+    res.json({
+      message: " Package Added",
+    });
+    res.status(200);
+    res.render("pages/index", { user: data });
+    return;
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.post("/logout", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let { data, error } = await supabase.auth.signOut();
+    ({
+      email,
+      password,
+    });
+    if (data) {
+      res.render("pages/signin", { user: data });
+      return;
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
