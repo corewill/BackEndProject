95170972d69559b67acbfcfa9760143db089e708
corewill <118204159+corewill@users.noreply.github.com>
2023-01-27 13:13:22 -0500

    Revert "endofday"
    This reverts commit 6ebefb953c95876a22c3f8fa61c96b5abe9044d8.

--- CarwashApp/index.js
+++ CarwashApp/index.js
@@ -1,47 +1,52 @@
 const { createClient } = require("@supabase/supabase-js");
-const supabaseUrl = "https://hrhviqtzmgmsttkvotvq.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyaHZpcXR6bWdtc3R0a3ZvdHZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ2NjIzNjYsImV4cCI6MTk5MDIzODM2Nn0.AiiB8o314C_rkOdneQk_vwvzxXO4W3lwHN-8-_NKEsA";
+const supabaseUrl = "https://pyymrehpabqxdbuobtbh.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5eW1yZWhwYWJxeGRidW9idGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ1OTA3MjgsImV4cCI6MTk5MDE2NjcyOH0.GPJW32brXyjktYJiinHapS82d4GVRlSUrv06gx9fBA0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const express = require("express")
 const app = express()
 const PORT = 3026
+// const bcrypt = require("bcrypt")
+// const session = require("express-session")
+// const cookieParser = require("cookie-parser")
+// const bodyParser = require("body-parser");
+
+
 app.use(express.json())
+
+// app.use(
+//     bodyParser.urlencoded({
+//         extended: true,
+//     })
+// )
+
+// app.use(bodyParser.json())
+// app.use(cookieParser())
+// app.use(session({
+//     secret: 'secret',
+//     resave: false,
+//     saveUninitialize: true,
+//     cookie: {
+//         secure: false,
+//         maxAge: 2592000000
+//     }
+// }))
 app.set("view engine", "ejs")
-app.use(express.static((__dirname +'public')));
 
 app.get("/login", (req, res) => {
     res.render("pages/login")
 })
-
-app.post("/signup", async (req, res) => {
-    const {email, password,} = req.body
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password
-    })
-    if(data){
-      res.redirect("login")
-      return
-    } else {
-      res.send("Error signing up")
-    }
-})
-
-app.post('/signin', async (req, res) => {
-  const { email, password } = req.body
-  try {
-    let { data, error } = await supabase.auth.signInWithPassword({
-      email ,
-      password
-    })
-    if(data){
-      res.render("pages/index",{user:data})
-      return
-    }
-  } catch (error) {
-      res.status(400).send(error)
-  }
+app.post("/login", async (req, res) => {
+    const {userName, passWord,firstName,lastName} = req.body
+    console.log(req.body)
+  console.log("route triggered")
+    const { data, error } = await supabase
+    .from('Members')
+    .insert([
+      { userName, firstName, lastName, passWord },
+    ])
+  console.log(error)
+    res.send(data)
 })
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`))
